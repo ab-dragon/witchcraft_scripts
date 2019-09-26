6b8aceae03684ce2e7c5abe51e091f7592a74698
arun bharadwaj <a.bharadwaj@idscan.com>
2019-09-26 22:54:59 +0100

    Running the operations after the window is fully rendered

--- idscangitlab.gb.co.uk.js
+++ idscangitlab.gb.co.uk.js
@@ -1,2 +1,4 @@
-document.getElementById("merge_request_force_remove_source_branch").checked = true;
-document.getElementById("merge_request_squash").checked = true;
+window.onload = function(){
+	document.getElementById("merge_request_force_remove_source_branch").checked = true;
+	document.getElementById("merge_request_squash").checked = true;
+}
