db8bb2714c95643e4fcad7478585533c544eaf7f
arun bharadwaj <a.bharadwaj@idscan.com>
2019-10-24 23:49:57 +0100

    Only allow to submit MR if atleast one label has been selected

--- idscangitlab.gb.co.uk.js
+++ idscangitlab.gb.co.uk.js
@@ -1,4 +1,17 @@
+// @include "jquery-3.4.1.min.js"
 window.onload = function(){
-	document.getElementById("merge_request_force_remove_source_branch").checked = true;
-	document.getElementById("merge_request_squash").checked = true;
-}
+    // check the squash and delete check boxes as soon as you enter the page
+    document.getElementById("merge_request_force_remove_source_branch").checked = true;
+    document.getElementById("merge_request_squash").checked = true;
+
+    // do not allow to submit a MR unless at least one label has been selected
+    var commit_btn = jQuery('[name="commit"]')
+    commit_btn[0].onmouseenter = function() {
+        if(commit_btn[0].value == "Save changes" || commit_btn[0].value == "Submit merge request") {
+            var lst = jQuery('[name="merge_request[label_ids][]"]');
+            if(lst.length < 2) {
+                window.alert("Please choose atlease one label for your MR");
+            }
+        }
+    }
+};
